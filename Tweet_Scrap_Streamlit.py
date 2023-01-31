8d446b59edcad329e10e757509d8a4844f71277e
vel97 <122588213+vel97@users.noreply.github.com>
2023-01-31 13:01:00 +0530

    Update Tweet_Scrap_Streamlit.py

--- Tweet_Scrap_Streamlit.py
+++ Tweet_Scrap_Streamlit.py
@@ -3,59 +3,80 @@
 import streamlit as st
 import pandas as pd
 import snscrape.modules.twitter as sntwitter
+import pymongo
+from pymongo import MongoClient
 
-#Declaring variables
-tweet_search = input()
-tweets_list = []
-
-#Pulling data from twitter
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper(tweet_search +" "+"since:2021-01-01 until:2021-05-31").get_items()):
-    if i>100:
-        break
-    tweets_list.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
-
+#Required functions
+def scrap_twitter(tweet_search, start_date, end_date, limit):
+    tweets_list = []
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper(tweet_search +" "+"since:"+str(start_date)+"until:"+str(end_date)).get_items()):
+        if i>limit:
+            break
+        tweets_list.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+    return tweets_list
+    
 #Storing the pulled data from twitter in a dataframe
-tweets_df = pd.DataFrame(tweets_list, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
-st.dataframe(tweets_df)
+def create_dataframe(tweets_list):
+    tweets_df = pd.DataFrame(tweets_list, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+    return tweets_df
 
-#To download the data in required format from streamlit app
-#CSV format
-@st.cache
+#Converting to CSV and JSON format
 def convert_csv(tweets_df):
     return tweets_df.to_csv().encode('utf-8')
 
-tweets_csv =convert_csv(tweets_df)
+def convert_json(tweets_df):
+    return tweets_df.to_json().encode('utf-8')
 
-st.download_button(
+#Declaring variables
+#Title
+st.title(':orange[Twitter Scrapper]')
+
+#Tweet to be searched
+tweet_search = st.text_input("Search hashtag")
+
+#Dates input
+start_date = st.date_input("Start date", key = "start_date")
+
+end_date = st.date_input("End date", key = "end_date")
+
+#No_of_tweets to be scrapped
+limit = st.number_input("Number of tweets required", key = "limit")
+
+#Scrapping the data
+if st.button("Scrap the data"):
+    tweets = scrap_twitter(tweet_search, start_date, end_date, limit)
+    tweets_df = create_dataframe(tweets)
+    st.dataframe(tweets)
+
+#To download the data in required format from streamlit app
+#CSV format
+    tweets_csv = convert_csv(tweets_df)
+    st.download_button(
     label="Download data as CSV",
-    data=tweets_csv,
+    data=tweets_df.to_csv(),
     file_name=tweet_search+".csv",
     mime='text/csv',
 )
 
 #JSON format
-@st.cache
-def convert_json(tweets_df):
-    return tweets_df.to_json().encode('utf-8')
-
-tweets_json = convert_json(tweets_df)
-st.download_button(
+    tweets_json = convert_json(tweets_df)
+    st.download_button(
     label="Download data as JSON",
-    data=tweets_json,
+    data=tweets_df.to_json(),
     file_name=tweet_search+".json",
     mime='json',
 )
 
-#Data collection in Mongodb from streamlit 
-import pymongo
-from pymongo import MongoClient
-
-#Using button from streamlit to push data to MongoDB
+#Pushing data to MongoDB
 if st.button('Push to Database'):
+    tweets = scrap_twitter(tweet_search, start_date, end_date, limit)
+    tweets_df = create_dataframe(tweets)
     pm = MongoClient("mongodb://Guser:Guvi.com3@ac-zstnwxd-shard-00-00.e8i7hjr.mongodb.net:27017,ac-zstnwxd-shard-00-01.e8i7hjr.mongodb.net:27017,ac-zstnwxd-shard-00-02.e8i7hjr.mongodb.net:27017/?ssl=true&replicaSet=atlas-9s52qi-shard-0&authSource=admin&retryWrites=true&w=majority")
     pm1 = pm['Twitter_data']
     data_dict = tweets_df.to_dict('records')
-    pmcollections1 = pm1[input()]
+    pmcollections1 = pm1[tweet_search]
     pmcollections1.insert_many(data_dict)
-    print('Data pushed to Database')
-    st.write('Data pushed to Database')
+    st.write('Data pushed to Database Succesfully')
+
+#Command to run the script.
+#''' streamlit run Tweet_Srap_Streamlit.py '''#
